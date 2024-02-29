454530708c3efa6232425b1a9769472c0e43659e
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2024-02-29 23:15:31 +0100

    chore: change port

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -2,9 +2,9 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { AppRouter } from "@server/trpc/trpc.router";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
-    }),
-  ],
+    links: [
+        httpBatchLink({
+            url: `https://localhost:4000/trpc`,
+        }),
+    ],
 });
