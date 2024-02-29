d534ca4a54ba00fdcf39a1867ab913f3aab76778
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2024-02-29 23:18:05 +0100

    fix: force dynamic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
 import Clientside from "./Clientside";
 import { trpc } from "./trpc";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
-  const response = await trpc.hello.query({});
-  return (
-    <div>
-      <p>Server side - {response}</p>
-      <Clientside />
-    </div>
-  );
+    const response = await trpc.hello.query({});
+    return (
+        <div>
+            <p>Server side - {response}</p>
+            <Clientside />
+        </div>
+    );
 }
