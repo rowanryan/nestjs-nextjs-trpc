704e08289486ab4a86f63347df97e7ac05708090
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2024-02-29 23:56:21 +0100

    please: last try

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,6 @@ const { join } = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-    output: "standalone",
     experimental: {
         outputFileTracingRoot: join(__dirname, "../../"),
     },
