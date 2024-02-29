d00e801fa3d9d317298a5130ee616d96eb4a1e14
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2024-02-29 23:37:26 +0100

    fix: add output file tracing

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { join } = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        outputFileTracingRoot: join(__dirname, "../../../.."),
+    },
+};
+
+module.exports = nextConfig;
